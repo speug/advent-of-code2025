821afc3f4db4985422a6c95c2f92249a056bdbf6
Lauri <you@example.com>
2025-12-11 19:25:21 +0200

    hnf done

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,5 +1,8 @@
 use core::fmt;
-use std::collections::{HashSet, VecDeque};
+use std::{
+    collections::{HashSet, VecDeque},
+    iter,
+};
 
 advent_of_code::solution!(10);
 
@@ -120,8 +123,165 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(minims)
 }
 
+type Matrix2D = Vec<Vec<i64>>;
+
+fn identity_matrix(n: usize) -> Matrix2D {
+    let mut out = vec![vec![0; n]; n];
+    for i in 0..n {
+        out[i][i] = 1;
+    }
+    out
+}
+
+fn extended_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if b == 0 {
+        if a < 0 {
+            return (-a, -1, 0);
+        } else {
+            return (a, 1, 0);
+        };
+    }
+    let (g, u1, v1) = extended_gcd(b, a.rem_euclid(b));
+    let u = v1;
+    let v = u1 - a.div_euclid(b) * v1;
+    (g, u, v)
+}
+
+fn swap_column(M: &mut Matrix2D, c1: usize, c2: usize) {
+    // matrix has columns for rows
+    M.swap(c1, c2);
+}
+
+fn update_column(M: &mut Matrix2D, col1: usize, col2: usize, u: i64, v: i64, c1: i64, c2: i64) {
+    for i in 0..M[0].len() {
+        let val_a = M[col1][i];
+        let val_b = M[col2][i];
+
+        M[col1][i] = (u * val_a) + (v * val_b);
+        M[col2][i] = (c1 * val_a) + (c2 * val_b);
+    }
+}
+
+fn multiply_column(M: &mut Matrix2D, col: usize, mult: i64) {
+    M[col].iter_mut().for_each(|x| *x *= mult);
+}
+
+fn subtract_column(M: &mut Matrix2D, col1: usize, col2: usize, factor: i64) {
+    for i in 0..M[0].len() {
+        M[col1][i] -= M[col2][i] * factor;
+    }
+}
+
+fn transpose2<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
+fn hnf(A: &Matrix2D) -> (Matrix2D, Matrix2D) {
+    let n = A.len();
+    let m = A[0].len();
+    let mut U = identity_matrix(m);
+    let mut H = transpose2(A.to_vec());
+
+    let mut current_col = 0;
+    for i in 0..n {
+        // 1. pivot selection; need a row with a non-zero elem right of curr_col
+        let mut pivot_col = None;
+        for k in current_col..m {
+            if H[k][i] != 0 {
+                pivot_col = Some(k);
+                break;
+            }
+        }
+
+        if pivot_col.is_none() {
+            continue;
+        }
+
+        let k = pivot_col.unwrap();
+        swap_column(&mut H, current_col, k);
+        swap_column(&mut U, current_col, k);
+
+        // 2. elimination
+        for j in current_col + 1..m {
+            if H[j][i] != 0 {
+                let pivot = H[current_col][i];
+                let target = H[j][i];
+
+                let (g, u, v) = extended_gcd(pivot, target);
+                let c1 = -target / g;
+                let c2 = pivot / g;
+
+                update_column(&mut H, current_col, j, u, v, c1, c2);
+                update_column(&mut U, current_col, j, u, v, c1, c2);
+            }
+        }
+
+        // 3. modulo reduction
+        let mut pivot_val = H[current_col][i];
+        if pivot_val < 0 {
+            multiply_column(&mut H, current_col, -1);
+            multiply_column(&mut U, current_col, -1);
+            pivot_val = -pivot_val;
+        }
+        for c in 0..current_col {
+            let factor = H[c][i].div_euclid(pivot_val);
+            subtract_column(&mut H, c, current_col, factor);
+            subtract_column(&mut U, c, current_col, factor);
+        }
+        current_col += 1;
+    }
+    (transpose2(H), transpose2(U))
+}
+
+fn actions_to_matrix(A: &Vec<Vec<usize>>, n: usize) -> Matrix2D {
+    let m = A.len();
+    let mut out = vec![vec![0; m]; n];
+    for (i, actions) in A.iter().enumerate() {
+        for &action in actions.iter() {
+            out[action][i] = 1;
+        }
+    }
+    out
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (_, actions, joltages) = parse_input(input);
+    let ji = 0;
+    let m = joltages[ji].len();
+    let a = actions_to_matrix(&actions[ji], m);
+
+    for row in a.iter() {
+        println!("{:?}", row);
+    }
+    println!("--------------");
+    let (h, u) = hnf(&a);
+    for row in h.iter() {
+        println!("{:?}", row);
+    }
+    println!("--------------");
+    for row in u.iter() {
+        println!("{:?}", row);
+    }
+    let mut y = vec![0; m];
+    for i in 0..m {
+        let mut hsum = 0;
+        for j in 0..i {
+            hsum += y[j] * h[i][j];
+        }
+        y[i] = (joltages[ji][i] as i64 - hsum) / h[i][i];
+    }
+    println!("Particular solution: {:?}", y);
+    Some(0)
 }
 
 #[cfg(test)]
